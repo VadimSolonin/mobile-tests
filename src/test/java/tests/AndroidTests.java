a21ff2f30fbf04f0c0c26e40053a78c81cc9595c
VS <solonin.vadim@mail.ru>
2023-12-03 14:05:46 +0300

    changed tests location

--- src/test/java/tests/AndroidTests.java
+++ src/test/java/tests/AndroidTests.java
@@ -1,7 +1,5 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
-import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -15,7 +13,7 @@ import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
-public class SearchTests extends TestBase {
+public class AndroidTests extends TestBase {
 
     @DisplayName("Проверка наличия элементов в результатах поиска")
     @Test
@@ -42,18 +40,5 @@ public class SearchTests extends TestBase {
                         .should(exist)
                         .shouldHave(text("Log in to Wikipedia"))
         );
-    }
-
-    @Test
-    @Tag("ios")
-    void verifyMainPageElementsExistingTest() {
-        step("Проверка отображения элементов на главной странице", () -> {
-            $(AppiumBy.className("XCUIElementTypeStaticText")).$(AppiumBy.name("UI Elements")).should(exist);
-            $(AppiumBy.name("Text Button")).shouldBe(Condition.visible);
-            $(AppiumBy.name("Text")).shouldBe(Condition.visible);
-            $(AppiumBy.name("Tab Bar")).should(exist);
-            $(AppiumBy.name("Web View")).should(exist);
-            $(AppiumBy.name("Local Testing")).should(exist);
-        });
     }
 }
